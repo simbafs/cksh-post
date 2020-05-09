232d0dd9479be939f2583abbb99746845522877e
simba-fs <simba.fs@gmail.com>
2020-05-09 20:49:43 +0800

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const boardcast = require('./boardcast.js');
 const cron = require('node-cron');
 
-cron('0 0 12 * * *', boardcast);
-cron('0 0 16 * * *', boardcast);
+cron.schedule('0 0 12 * * *', boardcast);
+cron.schedule('0 0 16 * * *', boardcast);
